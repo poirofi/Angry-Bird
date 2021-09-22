8c3159b32477aac99cbe115525b7e85f71b457cc
poirof <poirofirdaus@gmail.com>
2021-09-23 00:00:02 +0700

    Angry-Bird

---
+++ Assets/Scrips/BoomBird.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomBird : Bird
+{
+    public float explodeRadius = 2.00f;
+    public float force = 250f;
+    public LayerMask LayerToHit;
+
+    public GameObject explode;
+
+    void Explode()
+    {
+        Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, explodeRadius, LayerToHit);
+
+        foreach (Collider2D obj in objects)
+        {
+            Vector2 arah = obj.transform.position - transform.position;
+
+            obj.GetComponent<Rigidbody2D>().AddForce(arah * force);
+            if (obj.tag == "Enemy")
+            {
+                Enemy enemy = obj.GetComponent<Enemy>();
+                enemy.setHit();
+            }
+        }
+        GameObject explodebird = Instantiate(explode, transform.position, Quaternion.identity);
+        Destroy(explodebird, 5);
+    }
+
+    public override void OnCollisionEnter2D(Collision2D other)
+    {
+        Destroy(gameObject);
+        Explode();
+    }
+
+    //debug
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explodeRadius);
+    }
+
+}
---
+++ Assets/Scrips/WoodHealth.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class WoodHealth : MonoBehaviour
+{
+    public float Health = 50f;
+    public UnityAction<GameObject> OnWoodDestroyed = delegate { };
+    private bool _isHit = false;
+
+    void OnDestroy()
+    {
+        if (_isHit)
+        {
+            OnWoodDestroyed(gameObject);
+        }
+    }
+
+    public void setHit()
+    {
+        _isHit = true;
+        Destroy(gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        if (col.gameObject.tag == "Bird" || col.gameObject.tag == "Enemy")
+        {
+            //Hitung damage yang diperoleh
+            float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+            Health -= damage;
+
+            if (Health <= 0)
+            {
+                _isHit = true;
+                Destroy(gameObject);
+            }
+        }
+       
+    }
+}
